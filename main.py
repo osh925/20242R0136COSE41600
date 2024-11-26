cdd987fa3b1e95287c5fb360b802195e9ddd2717
osh925 <mickeyosh925@gmail.com>
2024-11-26 21:47:06 +0900

    commit5

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from model import visualize_pcd_sequence
 
-folders = ["01", "02", "03", "04", "05", "06", "07"]
+if __name__ == "__main__":
 
-for folder in folders:
-    folder_path = f"data/{folder}/pcd"
-    visualize_pcd_sequence(folder_path)
+    folders = ["01", "02", "03", "04", "05", "06", "07"]
+    for folder in folders:
+        folder_path = f"data/{folder}/pcd"
+        visualize_pcd_sequence(folder_path)
