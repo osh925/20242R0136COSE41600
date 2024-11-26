1c62ad2cf54943d96f78585512c7741cdd902c8e
osh925 <mickeyosh925@gmail.com>
2024-11-26 21:48:07 +0900

    commit6

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     folders = ["01", "02", "03", "04", "05", "06", "07"]
     for folder in folders:
         folder_path = f"data/{folder}/pcd"
+        
         visualize_pcd_sequence(folder_path)
